12e8e91224eaeac4c71b510bf6b0fbcf45f07822
Surya Kireeti Yarlagada <skyviswam@gmail.com>
2023-05-02 16:09:06 -0700

    Completed

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function on(){
+
+    document.getElementById('bulb').src='https://www.w3schools.com/js/pic_bulbon.gif'
+    
+    }
+
+    function off(){
+
+        document.getElementById('bulb').src='https://www.w3schools.com/js/pic_bulboff.gif'
+        console.log( "Bulb is off")
+        
+        }
